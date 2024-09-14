931c2e83702afa172a61dbb4a1a15907dd4279e2
MennaAllahZakaria <mennazakaria2003@gmail.com>
2024-09-14 17:06:53 +0300

    Authentication

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const userRoute=require('./userRoute');
+const authRoute=require('./authRoute');
 
 const mountRoutes=(app)=>{
-    app.use('/api/v1/users',userRoute)
+    app.use('/api/v1/users',userRoute);
+    app.use('/api/v1/auth',authRoute);
 
-}
+};
 
 module.exports=mountRoutes;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -28,19 +28,19 @@ const {
     
 }=require("../services/userService");
 
-// const{
-//     protect,
-//     allowedTo
-// }=require("../services/authService");
+const{
+    protect,
+    allowedTo
+}=require("../services/authService");
 
-// router.use(protect);
+router.use(protect);
 
 router.get('/getMe',getLoggedUserData,getUser);
 router.put('/changeMyPassword', updateLoggedUserPassword);
 router.put('/updateMe',updateLoggedUserValidator, updateLoggedUserData);
 router.delete('/deleteMe',deleteLoggedUserData);
 
-//router.use(allowedTo('admin','manager'));
+router.use(allowedTo('admin','manager'));
 router.put('/changePassword/:id',
             changeUserPasswordValidator,
             changeUserPassword
