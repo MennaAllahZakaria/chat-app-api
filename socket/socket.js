56b9786aec4158e8cf9be7eb927d2ecf952a0290
MennaAllahZakaria <mennazakaria2003@gmail.com>
2025-05-01 01:39:43 +0300

    auth socket

--- socket/socket.js
+++ socket/socket.js
@@ -1,9 +1,25 @@
+const jwt = require('jsonwebtoken');
 const joinRoom = require('./joinRoomService');
 const sendMessage = require('./sendMessageServise');
 const receiveMessage = require('./receiveMessageService');
 const typingIndicator = require('./typingIndicatorService');
 
 module.exports = (io) => {
+  io.use((socket, next) => {
+    const token = socket.handshake.auth?.token;
+    if (!token) {
+      return next(new Error('Authentication error'));
+    }
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      socket.user = decoded;
+      next();
+    } catch (err) {
+      next(new Error('Invalid token'));
+    }
+  });
+  
   io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
 
